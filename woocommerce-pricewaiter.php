4c2e08a36f51a66b6d0ec0f8472ec060dfb079d4
pricewaiter <pricewaiter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 20:39:18 +0000

    Resolves #18 by checking for WC 2.2.0 - Set the minimum WP version to 3.8 (WC 2.2.0 minimum). - Added admin_notice for WooCommerce missing or unsupported versions.
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-pricewaiter/trunk@1141239 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-pricewaiter.php
+++ woocommerce-pricewaiter.php
@@ -8,8 +8,13 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * Text Domain: woocommerce-pricewaiter
  * Author URI: http://pricewaiter.com/
  * Version: 0.0.1
+ * Requires at least: 3.8
  *
  */
+
+// Minimum supported version of WooCommerce
+$wc_minimum_version = '2.2.0';
+
 // Standard check if WooCommerce is active
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 	/**
@@ -41,6 +46,19 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 			);
 
 			public function __construct() {
+				add_action( 'plugins_loaded', array( $this, 'init' ) );
+			}
+
+			public function init() {
+				global $wc_minimum_version;
+
+				// We version_compare() after plugins_loaded()
+				// to make sure we have access to WC()->version
+				if( version_compare( WC()->version, $wc_minimum_version ) < 0 ) {
+					// Unsupported version
+					add_action( 'admin_notices' , array($this, 'alert_woocommerce_minimum_version') );
+					return;
+				}
 
 				// Include Required Files
 				$this->includes();
@@ -48,10 +66,6 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 				// Init API
 				$this->api = new WC_PriceWaiter_API();
 
-				add_action( 'plugins_loaded', array( $this, 'init' ) );
-			}
-
-			public function init() {
 				$this->supported_product_types = apply_filters( 'wc_pricewaiter_supported_product_types', $this->supported_product_types );
 				$this->supported_cost_plugins = apply_filters( 'wc_pricewaiter_supported_cost_plugins', $this->supported_cost_plugins );
 				
@@ -80,8 +94,35 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 				$integrations[] = 'WC_PriceWaiter_Integration';
 				return $integrations;
 			}
+
+			/**
+			* Notify Admin to update WooCommerce to minimum version
+			*/
+			public function alert_woocommerce_minimum_version(){
+				global $wc_minimum_version;
+				?>
+					<div class="error">
+						<h3><?php _e( 'WooCommerce update required to continue.', WC_PriceWaiter::TEXT_DOMAIN ); ?></h3>
+						<p><?php printf( __( "It appears you're currently using WooCommerce v%s", WC_PriceWaiter::TEXT_DOMAIN ), WC()->version ); ?></p>
+						<p><?php printf( __( "WooCommerce v%s or higher is required to use the WooCommerce PriceWaiter plugin.", WC_PriceWaiter::TEXT_DOMAIN ), $wc_minimum_version); ?></p>
+					</div>
+				<?
+			}
 		}
 
 		$GLOBALS['wc_pricewaiter'] = new WC_PriceWaiter( __FILE__ );
 	}
+} else {
+
+	function alert_woocommerce_required() {
+		global $wc_minimum_version;
+		?>
+			<div class="error">
+				<h3>WooCommerce required to continue.</h2>
+				<p><?php echo 'WooCommerce v' . $wc_minimum_version . ' or higher is required to use the WooCommerce PriceWaiter plugin.'; ?></p>
+				<p><a href="http://www.woothemes.com/woocommerce/">Install WooCommerce to get started.</a></p>
+			</div>
+		<?
+	}
+	add_action( 'admin_notices', 'alert_woocommerce_required' );
 }
