0dae8c130f80fe88d25488645ea2495889401f2d
pricewaiter <pricewaiter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-21 20:50:59 +0000

    Added uninstall.php to remove pw options
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-pricewaiter/trunk@1141296 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Uninstalling plugin
+ */
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+/**
+ * Remove any options defined buy the plugin
+ */
+delete_option( '_wc_pricewaiter_api_user_id' );
+delete_option( '_wc_pricewaiter_api_user_status' );
+delete_option( '_wc_pricewaiter_sign_up_token' );
+delete_option( 'woocommerce_pricewaiter_settings' );
